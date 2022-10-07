f813de7b17940806ea7235ac801b00635579a92f
nazarf <nazarfedisin@gmail.com>
2022-10-07 17:40:44 +0300

    init app and add first interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
 
+let tg = window.Telegram.WebApp;
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    useEffect(() => {
+        tg.ready()
+    }, [])
+
+    const onClose = () => {
+        tg.close();
+    }
+    return (
+        <div>
+            <button onClick={onClose}>
+                close
+            </button>
+        </div>
+    );
 }
 
 export default App;
